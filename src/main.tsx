3ebf069a942c268bed8b00a73c333d0cdd0477f6
WilliamHankey <wchankey15@gmail.com>
2026-03-30 13:47:43 +0200

    Added gtag

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ReactGA from "react-ga4";    
+
+const GA_ID = import.meta.env.REACT_APP_GA_MEASUREMENT_ID;
+
+if (GA_ID && process.env.NODE_ENV === "production") {
+  ReactGA.initialize(GA_ID);
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
